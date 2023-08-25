096c457668f74d97c71b9756dbc59c8b2eae47d3
Shiksha Yadav <shikshyadav81@gmail.com>
2023-08-25 23:06:39 +0545

    6th day

--- dataTypesummary.js
+++ dataTypesummary.js
@@ -40,3 +40,33 @@ console.log(typeof temperature); //null ko datatype is object.
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// STACK MEMORY (primitive type) HEAP MEMORY (Reference/non-primitive)
+
+
+let abc1 = "something";
+
+let abc2 = abc1;
+abc2 = "something else";
+
+console.log(abc1);
+console.log(abc2);              //in stack memory the copy of the first declared variable is given so the data doesnot change in stack first abc1 is stored than abc2 then the copy of abc1 goes up
+
+// HEAP MEMORY
+
+let user1 = {
+    email: "shikshyadav81@gmail.com",
+    phone: 98324245623  
+}
+
+let user2= user1;
+
+user2.email = "yadavshiksha@gmail.com"
+
+console.log(user1.email);
+console.log(user2.email);    //in heap memory the reference of original data is given i.e. if something is changed in the second object then the original object also changes
+                            // here the email of user1 gets changed if the email of user2 is changed
+
+
+
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,4 +1,11 @@
-let empty = "";
-console.log(empty.length);
-let space = " ";
-console.log(space.length);
+let user1 = {
+    email: "shikshyadav81@gmail.com",
+    phone: 98324245623  
+}
+
+let user2= user1;
+
+user2.email = "yadavshiksha@gmail.com"
+
+console.log(user1.email);
+console.log(user2.email);
